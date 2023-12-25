d0927c4b54a7f3747ba9704242364bed85c25435
Muralitharan171 <murran171@gmail.com>
2023-12-25 08:34:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -21,6 +21,9 @@ import airquality1 from "./Images/airquality1.png";
 import airquality2 from "./Images/airquality2.png";
 import airquality3 from "./Images/airquality3.png";
 import airquality4 from "./Images/airquality4.png";
+import leaf from "./Images/leaf.png";
+import wifi from "./Images/wifi.png";
+import wave from "./Images/wave.png";
 
 function App() {
   const settings = {
@@ -32,30 +35,70 @@ function App() {
     autoplay: true,         
     autoplaySpeed: 2000 
   };
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    const menuList = document.querySelector(".topbar ul");
+    if (menuList) {
+      menuList.classList.toggle("active-menu");
+      setMenuOpen(menuList.classList.contains("active-menu"));
+      console.log("MenuList class list:", menuList.classList.toString());
+    } else {
+      console.log("MenuList not found");
+    }
+  };
+
+  useEffect(() => {
+    const menuButton = document.querySelector(".menu-toggle");
+    if (menuButton) {
+      menuButton.addEventListener("click", handleClick);
+      console.log("Event listener added");
+    } else {
+      console.log("MenuButton not found");
+    }
+
+    return () => {
+      if (menuButton) {
+        menuButton.removeEventListener("click", handleClick);
+        console.log("Event listener removed");
+      }
+    };
+  }, []); 
+
+
   return (
-    <div className="App">
-      
-      <div className="app-container">
+   <div className="App">
+       <div className={`app-container ${menuOpen ? 'menu-open' : ''}`}>
+       <button type="submit" className="menu-toggle" onClick={() => handleClick()}>
+    <span className="trigram"></span>
+    Contact us
+</button>
   <div className="topbar">
     <nav>
-      <ul>
+      <ul className="menu-list">
         <a href="https://www.wiprolighting.com/products/new-offerings/aura-air"><i>Aura Air</i></a>
-        <li><a href="#home">Products</a></li>
-        <li><a href="#about">Technology</a></li>
-        <li><a href="#contact">Industries</a></li>
-        <li><a href="#about">Support</a></li>
-        <li><a href="#contact">Store</a></li>
-        <a href="#contact">Contact us</a> 
+        <li><a href="#products">Products</a></li>
+        <li><a href="#technology">Technology</a></li>
+        <li><a href="#industries">Industries</a></li>
+        <li><a href="#support">Support</a></li>
+        <li><a href="#store">Store</a></li>
+       
+
       </ul>
     </nav>
   </div>
   <div className="image-container">
     <div className="center-text">
       <b>Clean air, </b>
-      <b>everywhere</b>
+      <b>everywhere</b><br></br>
+      <button type="submit">Contact us</button>
     </div>
   </div>
 </div>
+
+
+
 
 
     <div className="logos-row">
@@ -68,29 +111,41 @@ function App() {
     </div>
 
 
-    <div class="container-1">
-  <div class="left-side">
+
+
+    <div className="container-1">
+  <div className="left-side">
     <h1>We are Aura Air</h1>
   </div>
-  <div class="right-side">
+  <div className="right-side">
     <p>We help improve people's health by helping them do the most basic,yet most important thing in life-breathe.our innovative air management system eliminates 99.9% of harmful particles while providing real-time data,insights,and recommendations.</p>
   </div>
 </div>
 
-<div class="container-2">
-  <div class="box">
-    <h2><span>&#x2605;</span> Earn Green Building Standars</h2>
+
+
+
+<div className="container-2">
+  <div className="box">
+    <img src={leaf} alt="leaf"/>
+    <h2> Earn Green Building Standars</h2>
     <p>Our easy plug-and-play solution provides 7 points for WELL,LEED,REST, and fitwell certifications.</p>
   </div>
-  <div class="box">
-    <h2><span>&#x2605;</span> BMS Connectivity</h2>
+  <div className="box">
+    <img src={wifi} alt="wifi"/>
+    <h2> BMS Connectivity</h2>
     <p>Connect Aura Air to your alexa,Google Assistant,IFTTT,or any other BMS for effortless operation.</p>
   </div>
-  <div class="box">
-    <h2><span>&#x2605;</span> Boost Your ESG Strategy</h2>
+  <div className="box">
+    <img src={wave} alt="wave"/>
+    <h2> Boost Your ESG Strategy</h2>
     <p>take advantage of single system to enhance all your environmental,social and goverrnance factors.</p>
   </div>
 </div>
+
+
+
+
 
 <h1 className="head">A Complete air quality process</h1>
   <p className="head">For indoor spaces</p>
@@ -100,23 +155,31 @@ function App() {
         <div className="row">
           <div className="cards one">
             <img src={airquality1} alt="airquality1" />
+            <h1 className="txt-1">24/7 Monitoring</h1>
+            <p className="txt-1">Tracks your indoor air quality with our innovitive device and smart sensord</p>
           </div>
         </div>
        
         <div className="row">
           <div className="cards two">
             <img src={airquality2} alt="airquality2" />
+            <h1 className="txt-2">Real-Time Data & Analysis</h1>
+            <p className="txt-2">See what's in your air and learn what factors control its quality</p>
           </div>
           
       </div>
       <div className="row">
           <div className="cards three">
             <img src={airquality3} alt="airquality3" />
+            <h1 className="txt-1">Smart Purification Process</h1>
+            <p className="txt-1">A 4 stage process that adapts according to its environment</p>
           </div>
       </div>
       <div className="row">
           <div className="cards four">
             <img src={airquality4} alt="airquality4" />
+            <h1 className="txt-2">Customized Insights</h1>
+            <p className="txt-2">Helping you manage your air quality with minimum effort and maximum protection</p>
           </div>
       </div>
       </div>
@@ -152,6 +215,9 @@ function App() {
     </div>
 
 
+
+
+
     <div className="card-carousel">
       <Slider {...settings}>
         <div className="card">
@@ -173,71 +239,73 @@ function App() {
         </Slider>
 </div>
 
+
+
+
+
 <div className="contact-us">
 <h1>Let's get started!</h1>
 <p>Join the future of air purification</p>
 <button type="submit">Contact Us</button>
 </div>
 
-<div class="footer">
+<div className="footer">
  
-  <div class="column">
-    <div class="row">WE ARE AURA AIR</div>
-    <div class="row">About</div>
-    <div class="row">Technology</div>
-    <div class="row">Blog</div>
-    <div class="row">News Room</div>
-    <div class="row">Careers</div>
+  <div className="column">
+    <div className="row">WE ARE AURA AIR</div>
+    <div className="row">About</div>
+    <div className="row">Technology</div>
+    <div className="row">Blog</div>
+    <div className="row">News Room</div>
+    <div className="row">Careers</div>
   </div>
 
  
-  <div class="column">
-    <div class="row">PRODUCTS</div>
-    <div class="row">Aura Air</div>
-    <div class="row">Aura Air Mini</div>
-    <div class="row">Aura Web Platform</div>
-    <div class="row">Accessories</div>
+  <div className="column">
+    <div className="row">PRODUCTS</div>
+    <div className="row">Aura Air</div>
+    <div className="row">Aura Air Mini</div>
+    <div className="row">Aura Web Platform</div>
+    <div className="row">Accessories</div>
   </div>
 
   
-  <div class="column">
-    <div class="row">INDUSTRIES</div>
-    <div class="row">Commercial Real Estate</div>
-    <div class="row">Residential Real Estate</div>
-    <div class="row">Education</div>
-    <div class="row">Transportation</div>
-    <div class="row">Health Care</div>
+  <div className="column">
+    <div className="row">INDUSTRIES</div>
+    <div className="row">Commercial Real Estate</div>
+    <div className="row">Residential Real Estate</div>
+    <div className="row">Education</div>
+    <div className="row">Transportation</div>
+    <div className="row">Health Care</div>
   </div>
 
 
-  <div class="column">
-    <div class="row">COMPANY</div>
-    <div class="row">Support</div>
-    <div class="row">Become A Distributor</div>
-    <div class="row">Returns Policy</div>
-    <div class="row">Investor Relation</div>
+  <div className="column">
+    <div className="row">COMPANY</div>
+    <div className="row">Support</div>
+    <div className="row">Become A Distributor</div>
+    <div className="row">Returns Policy</div>
+    <div className="row">Investor Relation</div>
   </div>
 
 
-  <div class="column">
-    <div class="row">LET'S BE FRIENDS!</div>
-    <div class="input-container">
+  <div className="column">
+    <div className="row">LET'S BE FRIENDS!</div>
+    <div className="input-container">
   <input type="text" placeholder="Email address" />
-  <div class="arrow">&#9658;</div>
+  <div className="arrow">&#9658;</div>
 </div>
   </div>
-  
-
-   
   </div>
-  <div class="line"></div>
+  <div className="line"></div>
   
-  <div class="additional-row">
-  <p class="para">2022.Aura Smart Air INC.Aura Smart Air LTD</p>
+  <div className="additional-row">
+  <p className="para-end">2022.Aura Smart Air INC.Aura Smart Air LTD</p>
   <a href="terms">Terms</a>
   <a href="privacy">Privacy</a>
   <a href="accessibility">Accessibility</a>
- 
+
+
   </div>
     </div>
    
